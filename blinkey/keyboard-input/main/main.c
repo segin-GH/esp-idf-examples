f18f689c3560a61cbe26a98dedf62ababf593516
segin-GH <segin.bytes@gmail.com>
2022-09-30 08:59:31 +0530

    .

--- blinkey/keyboard-input/main/main.c
+++ blinkey/keyboard-input/main/main.c
@@ -6,19 +6,24 @@
 
 void app_main(void)
 {
-    char c = 0;
-    char str[100];
-    memset(str,0,sizeof(str));
-
-    while(c != '\n')
+    while(true)
     {
-        c = getchar();
-        if (getchar() != 0xff)
+        char c = 0;
+        char str[100];
+        memset(str,0,sizeof(str));
+
+        while(c != '\n')
         {
-            str[strlen(str)] = c;
-            printf("%c",c);
+            c = getchar();
+            if (c != 0xff)
+            {
+                str[strlen(str)] = c;
+                printf("%c",c);
+            }
+            vTaskDelay(100/portTICK_PERIOD_MS);
         }
-        vTaskDelay(1000/portTICK_PERIOD_MS);
+
+        printf("you typed :%s\n",str);
     }
-    printf("you typed %s\n",str);
 }
+
