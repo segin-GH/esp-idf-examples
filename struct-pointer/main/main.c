2c3e9d23cef480588d1cf2a5db65ba135be4ee33
segin-GH <segin.bytes@gmail.com>
2022-09-30 12:12:50 +0530

    .

--- struct-pointer/main/main.c
+++ struct-pointer/main/main.c
@@ -8,14 +8,31 @@ typedef struct struct_person
     int age;
 }Person;
 
+void updatePerson(Person *person1);
+void addecxlam(char *word);
 
 void app_main(void)
 {
     Person person1;
-    strcpy(person1.firstName,"bob");
-    strcpy(person1.lastName,"fisher");
-    person1.age = 35;
+    updatePerson(&person1);
+    char word[20] = {"hello World"};
+    addecxlam(word);
+    printf("%s\n",word);
     printf("person: %s %s is %d years old \n",person1.firstName,
             person1.lastName,person1.age);
 
+}
+
+
+void addecxlam(char *word)
+{
+    const char ex[] = "!";
+    strcat(word,ex);
+}
+
+void updatePerson(Person *person1)
+{
+    strcpy(person1->firstName,"bob");
+    strcpy(person1->lastName,"fisher");
+    person1->age = 35;
 }
