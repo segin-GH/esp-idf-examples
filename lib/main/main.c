17e447d0a1bb38c8e0bb2b3cfd4b1e1a213da757
segin-GH <segin.bytes@gmail.com>
2022-09-30 17:12:56 +0530

    .

---
+++ lib/main/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "module.h"
+
+void app_main(void)
+{
+    int ans = addint(10,20);
+    printf("%i,\n",ans);
+}
---
+++ lib/main/module.c
@@ -0,0 +1,6 @@
+
+
+    int addint(int x, int y)
+    {
+        return x + y; 
+    }
