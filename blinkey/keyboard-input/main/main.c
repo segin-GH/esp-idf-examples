25a6b9e57251603a58d6b709a4fe3344dc884c3e
segin-GH <segin.bytes@gmail.com>
2022-09-30 08:33:52 +0530

    .

---
+++ blinkey/keyboard-input/main/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+
+void app_main(void)
+{
+    char c = 0;
+    char str[100];
+    memset(str,0,sizeof(str));
+
+    while(c != '\n')
+    {
+        c = getchar();
+        if (getchar() != 0xff)
+        {
+            str[strlen(str)] = c;
+            printf("%c",c);
+        }
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
+    printf("you typed %s\n",str);
+}
