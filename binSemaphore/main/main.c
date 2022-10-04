2f48d248a2f489b06866c4e4b1e3d2f4bf844e1a
segin-GH <segin.bytes@gmail.com>
2022-10-04 17:33:52 +0530

    .

--- binSemaphore/main/main.c
+++ binSemaphore/main/main.c
@@ -1,6 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/semphr.h>
+
+xSemaphoreHandle binsemaph;
+
+void listenForHTTP()
+{
+    for(;;)
+    {
+        printf("recived https message\n");
+        xSemaphoreGive(binsemaph);        
+        vTaskDelay(5000/portTICK_PERIOD_MS);
+        printf(" processed \n");
+    }
+}
+
+void task1(void *parms)
+{
+    for(;;)
+    {
+        xSemaphoreTake(binsemaph,portMAX_DELAY);
+        printf("doing something with http");
+    }
+}
 
 void app_main(void)
 {
 
+    binsemaph = xSemaphoreCreateBinary();
+
+    xTaskCreatePinnedToCore(
+       listenForHTTP,
+       "listenForHTTP",
+       1024,
+       NULL,
+       2,
+       NULL,
+       APP_CPU_NUM 
+    );
+
+    xTaskCreatePinnedToCore(
+        task1,
+        "task1",
+        1024,
+        NULL,
+        2,
+        NULL,
+        APP_CPU_NUM
+    );
 }
