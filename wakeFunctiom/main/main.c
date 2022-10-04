abf0c8ab25724fae3093bd0f74a79cd73f40e04c
segin-GH <segin.bytes@gmail.com>
2022-10-04 09:38:20 +0530

    .

--- wakeFunctiom/main/main.c
+++ wakeFunctiom/main/main.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+static TaskHandle_t waker = NULL;
+
+void WakerFunction(void *parms)
+{
+    for(;;)
+    {
+        xTaskNotifyGive(waker);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
+}
+
+void functionWhichIsSleep(void *parms)
+{
+    for(;;)
+    {
+        ulTaskNotifyTake(pdFALSE,portMAX_DELAY);
+        printf("Function is awake.\n");
+    }
+}
 
 void app_main(void)
 {
+    xTaskCreatePinnedToCore(
+        functionWhichIsSleep,
+        "functionWhichIssleep",
+        1024,
+        NULL,
+        1,
+        &waker,
+        APP_CPU_NUM
+    );
 
+    xTaskCreatePinnedToCore(
+        WakerFunction,
+        "wakerFunction",
+        1024,
+        NULL,
+        1,
+        NULL,
+        APP_CPU_NUM
+    );
 }
