313b1cc1f83a16a4a7bf843957585e3b5f02c50b
segin-GH <segin.bytes@gmail.com>
2022-10-05 23:25:12 +0530

    .

--- high-resolution-Timer/main/main.c
+++ high-resolution-Timer/main/main.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <esp_timer.h>
 
-esp_timer_handle_t espTimmerHandle;
 
 void timer_callback(void *args)
 {
-    printf("this if fired after 20us");
+    printf("this if fired after 20us\n");
 }
 
 void app_main(void)
 {
     const esp_timer_create_args_t esp_timer_create_args = {
-        .callback = timer_callback
+        .callback = timer_callback,
         .name = "My timmer"
-
     };
-    esp_timer_create(esp_timer_create_args,&espTimmerHandle);
+    esp_timer_handle_t espTimmerHandle;
+    esp_timer_create(&esp_timer_create_args,&espTimmerHandle);
     esp_timer_start_once(espTimmerHandle,20);
     
 }
