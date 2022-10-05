385bcba59482e8737c251e02bea094f942f5d7da
segin-GH <segin.bytes@gmail.com>
2022-10-05 17:08:47 +0530

    .

--- Event-group/main/main.c
+++ Event-group/main/main.c
@@ -1,6 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/event_groups.h>
 
-void app_main(void)
+EventGroupHandle_t  evtgrp;
+const int gotHTTP = BIT0;
+const int gotBLE = BIT1;
+
+void listenToHTTPs (void *parms)
 {
+    for(;;)
+    {
+        xEventGroupSetBits(evtgrp,gotHTTP);
+        printf("got HTTPS\n");
+        vTaskDelay(2000/portTICK_PERIOD_MS);
+    }
+}
 
+void listenToBluetooh (void *parms)
+{
+    for(;;)
+    {
+        xEventGroupSetBits(evtgrp,gotBLE);
+        printf("got BLE\n");
+        vTaskDelay(5000/portTICK_PERIOD_MS);
+    }
+}
+
+void Task1 (void *parms)
+{
+    for(;;)
+    {
+        xEventGroupWaitBits(evtgrp, gotHTTP | gotBLE, true, true, portMAX_DELAY);
+        printf("recived HTTP and BLE\n");
+    }
+}
+
+void app_main (void)
+{
+    evtgrp = xEventGroupCreate();
+
+    xTaskCreatePinnedToCore(
+        listenToHTTPs,
+        "listenToHTPPs",
+        2048,
+        NULL,
+        2,
+        NULL,
+        APP_CPU_NUM
+    );
+
+    xTaskCreatePinnedToCore(
+        listenToBluetooh,
+        "listenToBluetooh",
+        2048,
+        NULL,
+        2,
+        NULL,
+        APP_CPU_NUM
+    );
+
+    xTaskCreatePinnedToCore(
+        Task1,
+        "task1",
+        2048,
+        NULL,
+        2,
+        NULL,
+        APP_CPU_NUM
+    );
 }
