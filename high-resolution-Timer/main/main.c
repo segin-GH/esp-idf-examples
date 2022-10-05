f98469536968360d7b9684ba942126a92e2b1b9c
segin-GH <segin.bytes@gmail.com>
2022-10-05 23:16:15 +0530

    .

--- high-resolution-Timer/main/main.c
+++ high-resolution-Timer/main/main.c
@@ -5,7 +5,7 @@ esp_timer_handle_t espTimmerHandle;
 
 void timer_callback(void *args)
 {
-    //
+    printf("this if fired after 20us");
 }
 
 void app_main(void)
@@ -14,8 +14,8 @@ void app_main(void)
         .callback = timer_callback
         .name = "My timmer"
 
-    }
+    };
     esp_timer_create(esp_timer_create_args,&espTimmerHandle);
-    esp_timer_start_once(espTimmerHandle,)
+    esp_timer_start_once(espTimmerHandle,20);
     
 }
