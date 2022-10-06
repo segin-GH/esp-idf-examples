54787911c8387c16e20c55eb9d33e48e155049f0
segin-GH <segin.bytes@gmail.com>
2022-10-06 11:59:15 +0530

    .

--- custm-config/main/main.c
+++ custm-config/main/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <esp_log.h>
+
+#define TAG "CONFIG"
 
 void app_main(void)
 {
-
+    ESP_LOGI(TAG,"MY_INT %d", CONFIG_MY_INT);
+    ESP_LOGI(TAG,"MY_STRING %s", CONFIG_MY_STRING);
+    
 }
