0ece02e2163f8188cad95952100d84110422f7fc
segin-GH <segin.bytes@gmail.com>
2022-10-06 19:01:17 +0530

    .

--- files-2-flash/main/component.mk
+++ files-2-flash/main/component.mk
@@ -2,3 +2,8 @@
 # "main" pseudo-component makefile.
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
+
+COMPONENT_EMBED_TXTFILES := files/idex.html
+COMPONENT_EMBED_TXTFILES += files/sample.txt
+COMPONENT_EMBED_FILES := files/pinout.jpg
+
