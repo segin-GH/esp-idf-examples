ce4986878c4733ec7854226ec36fb8e546d28b00
segin-GH <segin.bytes@gmail.com>
2022-10-08 23:38:13 +0530

    .

--- gpio-input/main/main.c
+++ gpio-input/main/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <driver/gpio.h>
+
+    int switchPin = 15;
+    int ledPin = 2;
 
 void app_main(void)
 {
+    gpio_pad_select_gpio(switchPin);
+    gpio_pad_select_gpio(ledPin);
+
+    gpio_set_direction(ledPin,GPIO_MODE_OUTPUT);
+    gpio_set_direction(switchPin,GPIO_MODE_INPUT);
+
+    gpio_pulldown_en(switchPin);
+
+    while(true)
+    {
+        int level = gpio_get_level(switchPin);
+        gpio_set_level(ledPin,level);
+        vTaskDelay(1);
+    }
 
 }
