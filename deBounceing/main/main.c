f72e56fe9dc5a8ae9d4ab663a187e2d8067908e2
segin-GH <segin.bytes@gmail.com>
2022-10-09 21:38:16 +0530

    .

--- deBounceing/main/main.c
+++ deBounceing/main/main.c
@@ -28,19 +28,36 @@ void when_switch_is_pushed(void *args)
     {
         if(xQueueReceive(interputQueue,&pinNumber,portMAX_DELAY))
         {
+            gpio_isr_handler_remove(pinNumber);
+            do 
+            {
+                vTaskDelay(20/portTICK_PERIOD_MS);
+            }
+            while(gpio_get_level(pinNumber) == 1);
+
             printf(" gpio %d was pressed %d times, the state is %d \n",pinNumber,count++,
             gpio_get_level(switchPin));
+            gpio_isr_handler_add(switchPin,gpio_isr_handler,(void *)switchPin);
         }
     }
 }
 
 void app_main(void)
 {
-    gpio_set_direction(switchPin,GPIO_MODE_INPUT);
-    gpio_set_direction(ledPin,GPIO_MODE_INPUT);
+    // gpio_set_direction(switchPin,GPIO_MODE_INPUT);
+    // gpio_set_direction(ledPin,GPIO_MODE_INPUT);
     
-    gpio_pulldown_en(switchPin);
-    gpio_set_intr_type(switchPin,GPIO_INTR_POSEDGE);
+    // gpio_pulldown_en(switchPin);
+    // gpio_set_intr_type(switchPin,GPIO_INTR_POSEDGE);
+
+    gpio_config_t config;
+        config.intr_type = GPIO_INTR_POSEDGE;
+        config.mode = GPIO_MODE_INPUT;
+        config.pull_down_en = 1;
+        config.pull_down_en = 0;
+        config.pin_bit_mask = (1ULL << switchPin);
+    gpio_config(&config);
+
 
     interputQueue = xQueueCreate(intrQueuLen,sizeof(int));
     
