f07e23dfb113be6510b932ef3971e1ecac3e8c85
segin <98380527+segin-GH@users.noreply.github.com>
2022-10-13 18:38:27 +0530

    Update main.c

--- uart/main/main.c
+++ uart/main/main.c
@@ -33,9 +33,135 @@ void app_main(void)
 
 }
 
+/*
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "driver/uart.h"
+#include "freertos/queue.h"
+#include "esp_log.h"
+#include "soc/uart_struct.h"
+
+#define ECHO_TEST_TXD  (4)
+#define ECHO_TEST_RXD  (5)
+
+#define BUF_SIZE (1024)
+
+//an example of echo test without hardware flow control on UART1
+static void echo_task()
+{
+    const int uart_num = UART_NUM_1;
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,    
+        .rx_flow_ctrl_thresh = 122,
+    };
+    //Configure UART1 parameters
+    uart_param_config(uart_num, &uart_config);
+
+    //Set UART1 pins(TX: IO4, RX: I05)
+    uart_set_pin(uart_num, ECHO_TEST_TXD, ECHO_TEST_RXD, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    
+    //Install UART driver (we don't need an event queue here)
+    //In this example we don't even use a buffer for sending data.
+    uart_driver_install(uart_num, BUF_SIZE * 2, 0, 0, NULL, 0);
+
+    uint8_t* data = (uint8_t*) malloc(BUF_SIZE);
+    while(1) {
+        //Read data from UART
+        int len = uart_read_bytes(uart_num, data, BUF_SIZE, 20 / portTICK_RATE_MS);
+        //Write data back to UART
+        uart_write_bytes(uart_num, (const char*) data, len);
+    }
+}
+
+void app_main()
+{
+    //A uart read/write example without event queue;
+    xTaskCreate(echo_task, "uart_echo_task", 1024, NULL, 10, NULL);
+}
 
 
 
+*/
+
+/* 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "driver/uart.h"
+#include "freertos/queue.h"
+#include "esp_log.h"
+#include "soc/uart_struct.h"
+
+#define ECHO_TEST_TXD  (4)
+#define ECHO_TEST_RXD  (5)
+
+#define BUF_SIZE (1024)
+
+
+//an example of echo test with hardware flow control on UART1
+static void echo_task()
+{
+    const int uart_num0 = UART_NUM_0;
+    uart_config_t uart_config0 = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,    //UART_HW_FLOWCTRL_CTS_RTS,
+        .rx_flow_ctrl_thresh = 122,
+    };
+
+    const int uart_num1 = UART_NUM_1;
+    uart_config_t uart_config1 = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,    //UART_HW_FLOWCTRL_CTS_RTS,
+        .rx_flow_ctrl_thresh = 122,
+    };
+    //Configure UART1 parameters
+    uart_param_config(uart_num0, &uart_config0);
+    uart_param_config(uart_num1, &uart_config1);
+
+    uart_set_pin(uart_num0, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    uart_set_pin(uart_num1, ECHO_TEST_TXD, ECHO_TEST_RXD, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    
+    //Install UART driver (we don't need an event queue here)
+    //In this example we don't even use a buffer for sending data.
+    uart_driver_install(uart_num0, BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_driver_install(uart_num1, BUF_SIZE * 2, 0, 0, NULL, 0);
+
+    uint8_t* data = (uint8_t*) malloc(BUF_SIZE);
+    while(1) {
+        //Read data from UART
+        int len = uart_read_bytes(uart_num0, data, BUF_SIZE, 20 / portTICK_RATE_MS);
+        //Write data back to UART
+        uart_write_bytes(uart_num1, (const char*) data, len);
+        uart_write_bytes(uart_num0, (const char*) data, len);
+    }
+}
+
+void app_main()
+{
+    //A uart read/write example without event queue;
+    xTaskCreate(echo_task, "uart_echo_task", 1024, NULL, 10, NULL);
+}
+*/
 
 
 
