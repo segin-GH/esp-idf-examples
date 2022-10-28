0bf33b5d13826ccac646c65d4898de9c31944d32
segin-GH <segin.bytes@gmail.com>
2022-10-28 16:01:55 +0530

    .

--- timestamp/main/main.c
+++ timestamp/main/main.c
@@ -7,12 +7,12 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-void get_time_stamp(char *buffer)
+struct timeval tv;
+
+void get_time_stamp(char *buffer, struct timeval tv_now)
 {
-    struct timeval tv_now;
     gettimeofday(&tv_now, NULL);
-    sprintf(buffer,"%ld:%ld:",(tv_now.tv_usec), tv_now.tv_sec);
-    
+    sprintf(buffer,"%ld:",(unsigned long )time(NULL));
 }
 
 void app_main()
@@ -20,9 +20,16 @@ void app_main()
     char str[100];
     char dataBuf[] = {"data"};
     memset(str,0,sizeof(str));
+
+    tv.tv_sec = 1666945715;
+    tv.tv_usec = 0;
+    int err = settimeofday(&tv,NULL);
+    if(err != 0)
+    esp_restart();
+
     while(true)
     {
-        get_time_stamp(str);
+        get_time_stamp(str,tv);
         strcat(str,dataBuf);
         printf("%s\n",str);
         vTaskDelay(1000/portTICK_PERIOD_MS);
