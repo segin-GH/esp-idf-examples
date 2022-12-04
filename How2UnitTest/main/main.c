6a3f1ed3d3bd5e0efd9503b2e52a2d0dac460f9a
segin-GH <segin.bytes@gmail.com>
2022-12-04 13:48:05 +0530

    .

--- How2UnitTest/main/main.c
+++ How2UnitTest/main/main.c
@@ -6,13 +6,11 @@ void app_main(void)
     UNITY_BEGIN();
     unity_run_test_by_name("sprintf");
     UNITY_END();
-
 }
 
 TEST_CASE("sprintf", "[NoFormatOperations]")
 {
     char output[5];
-
     TEST_ASSERT_EQUAL(4, sprintf(output, "hey"));
     TEST_ASSERT_EQUAL_STRING("hey", output);
 }
