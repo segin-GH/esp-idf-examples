ed071ced2b0877b90c095720e991452863d6bdbd
segin-GH <segin.bytes@gmail.com>
2022-12-18 01:46:44 +0530

    .

--- bitshifing/main/main.c
+++ bitshifing/main/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-uint16_t memoryAddr = 0b0001;
+uint16_t memoryAddr = 0b00000000;
 
 void print_binary(unsigned int num) 
 {
   int i;
 
-  for (i = 3; i >= 0; i--) {
+  for (i = 7; i >= 0; i--) {
     printf("%d", (num >> i) & 1);
   }
   printf("\n");
@@ -15,9 +15,17 @@ void print_binary(unsigned int num)
 
 void app_main(void)
 {
-    for(int i = 0; i < 4; i++)
-    {
-        memoryAddr = memoryAddr << 1;
-        print_binary(memoryAddr);
-    }
+    // for(int i = 0; i < 4; i++)
+    // {
+    //     memoryAddr = memoryAddr << 1;
+    //     print_binary(memoryAddr);
+    // }
+
+    // memoryAddr |= 1 << 0;
+    // memoryAddr |= 1 << 1;
+    // memoryAddr |= 1 << 2;
+    memoryAddr |= 1 << 4;
+
+    print_binary(memoryAddr);
+
 }
