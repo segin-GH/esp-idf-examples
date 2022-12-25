2adf2ef3bd9eb507879462a47215f8b4eac5c61e
segin-GH <segin.bytes@gmail.com>
2022-12-25 20:46:46 +0530

    added a folder of index.html, index.js, style.css

--- otafromaWebsite/site/index.js
+++ otafromaWebsite/site/index.js
@@ -1,1 +1,29 @@
-//nothing to implement
+const form = document.forms["otaForm"];
+const fileInput = document.getElementById("fileInput");
+const URL = "http://esp-server.local/update";
+
+let file;
+
+console.log("my dear monu")
+fileInput.addEventListener("change", () => {
+  file = fileInput.files[0];
+});
+
+const uploadFile = (file) => {
+  const fd = new FormData();
+  fd.append("otaFile", file);
+  console.log(fd);
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "URL");
+  xhr.upload.addEventListener("progress", ({ loaded, total }) => {
+    const fileLoaded = Math.floor((loaded / total) * 100);
+    const progress = document.getElementById("progress-percentage");
+    progress.innerHTML = `${fileLoaded}%`;
+  });
+  xhr.send(fd);
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  uploadFile(file);
+});
