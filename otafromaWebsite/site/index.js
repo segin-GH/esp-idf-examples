c311a41221772af679ab376b6f6c1785d832cdc8
segin-GH <segin.bytes@gmail.com>
2022-12-28 13:22:12 +0530

    fixed error where bin and spiffs file was not able to upload through website

--- otafromaWebsite/site/index.js
+++ otafromaWebsite/site/index.js
@@ -1,12 +1,13 @@
 const form = document.forms["otaForm"];
 const fileInput = document.getElementById("fileInput");
 let file;
-let checkedValue;
+let checkedValue="binary";
 
 const radioButtons = document.getElementsByName("updateType");
 for (let i = 0; i < radioButtons.length; i++) {
   radioButtons[i].addEventListener("change", () => {
     checkedValue = radioButtons[i].value;
+    document.getElementById("progress-percentage").innerHTML = `0%`;
   });
 }
 
@@ -15,37 +16,11 @@ fileInput.addEventListener("change", () => {
 });
 
 const uploadFile = (file) => {
-  // Create an XMLHttpRequest object
+
   const xhr = new XMLHttpRequest();
-
-  // Declare variables to store the form data and the endpoint
-  let formData;
-  let endpoint;
-  let contentType;
-
-  // Check the value of checkedValue and set the form data, endpoint, and content type accordingly
-  if (checkedValue === "binary") {
-    formData = file;
-    endpoint = "/ota";
-    contentType = "application/octet-stream";
-  } else {
-    // Create a FormData object
-    formData = new FormData();
-    // Append the file to the FormData object
-    formData.append("textFile", file);
-    endpoint = "/spiffs";
-    contentType = "multipart/form-data";
-  }
-
-  xhr.open("POST", endpoint);
-  xhr.setRequestHeader("Content-Type", contentType);
-
-  // Add an event listener for the "progress" event
-  xhr.upload.addEventListener("progress", (event) => {
-    const progress = Math.floor((event.loaded / event.total) * 100);
-    document.getElementById("progress-percentage").innerHTML = `${progress}%`;
-  });
-
+if(checkedValue==="binary"){
+  xhr.open("POST", "/ota");
+  xhr.setRequestHeader("Content-Type", "application/octet-stream");
   xhr.onload = () => {
     // Check the status of the response
     if (xhr.status === 200) {
@@ -54,12 +29,35 @@ const uploadFile = (file) => {
     } else {
       console.error("Error uploading file!");
       window.alert(xhr.responseText);
-    }
-  };
-
-  // Send the request
+    } };
+    xhr.upload.addEventListener("progress", (event) => {
+      const progress = Math.floor((event.loaded / event.total) * 100);
+      document.getElementById("progress-percentage").innerHTML = `${progress}%`;
+    }); 
+  xhr.send(file);
+  console.log(xhr.responseText);
+  
+}else if( checkedValue==="fs"){
+  xhr.open("POST", "/spiffs");
+  formData = new FormData();
+ formData.append("textFile", file);
+  xhr.setRequestHeader("Content-Type", "multipart/form-data");
+  xhr.onload = () => {
+    // Check the status of the response
+    if (xhr.status === 200) {
+      console.log(xhr.responseText);
+      window.alert(xhr.responseText);
+    } else {
+      console.error("Error uploading file!");
+      window.alert(xhr.responseText);
+    } };
+    xhr.upload.addEventListener("progress", (event) => {
+      const progress = Math.floor((event.loaded / event.total) * 100);
+      document.getElementById("progress-percentage").innerHTML = `${progress}%`;
+    }); 
   xhr.send(formData);
   console.log(xhr.responseText);
+}
 };
 
 form.addEventListener("submit", (e) => {
