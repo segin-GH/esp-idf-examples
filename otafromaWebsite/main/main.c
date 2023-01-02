33c8bbbe6c401ed20cfaf45511ef4d61ee853df6
segin-GH <segin.bytes@gmail.com>
2023-01-02 17:43:16 +0530

    implemented new wifi components

--- otafromaWebsite/main/main.c
+++ otafromaWebsite/main/main.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <ota.h>
+#include <nvs_flash.h>
 #include "wifi_connect.h"
+
 
 wifi_cred_t wifi_cred = {
     .wifi_name = "Segin",
-    .wifi_pass = "sejin2003",
+    .wifi_pass = "2003sejin",
     .k_timeout = 10000
 };
 
 void app_main(void)
 {
+    nvs_flash_init();
+    wifi_init();
     wifi_connect_sta(&wifi_cred);
     set_mDNS_name(NULL); // new addr will be http://pdb-esp.local/
     init_ota();
