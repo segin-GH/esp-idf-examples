8027dd225dcc52fdfad45531ca3b19bbfde25267
segin-GH <segin.bytes@gmail.com>
2023-03-20 17:16:21 +0530

    .

--- TaskNotification-2/main/main.c
+++ TaskNotification-2/main/main.c
@@ -6,49 +6,46 @@
 
 static TaskHandle_t receiverHandler = NULL;
 
-void sender(void * params)
+void sender(void *params)
 {
-    while (true)
-    {
-      xTaskNotify(receiverHandler,(1 << 0),eSetValueWithOverwrite);
-      vTaskDelay(5000 / portTICK_PERIOD_MS);
-      xTaskNotify(receiverHandler,(0x02),eSetValueWithOverwrite);
-      vTaskDelay(5000 / portTICK_PERIOD_MS);
-      xTaskNotify(receiverHandler,(0b0011),eSetValueWithOverwrite);
-      vTaskDelay(5000 / portTICK_PERIOD_MS);
-      xTaskNotify(receiverHandler,(0x04),eSetValueWithOverwrite);
-      vTaskDelay(5000 / portTICK_PERIOD_MS);
-    }
-    
+  while (true)
+  {
+    xTaskNotify(receiverHandler, (1 << 0), eSetValueWithOverwrite);
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+    xTaskNotify(receiverHandler, (0x02), eSetValueWithOverwrite);
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+    xTaskNotify(receiverHandler, (0b0011), eSetValueWithOverwrite);
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+    xTaskNotify(receiverHandler, (0x04), eSetValueWithOverwrite);
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+  }
 }
 
-void receiver(void * params) 
+void receiver(void *params)
 {
   uint state;
   while (true)
   {
-    xTaskNotifyWait(0,0,&state,portMAX_DELAY);
-    printf("received state %d times\n",state);
+    xTaskNotifyWait(0, 0, &state, portMAX_DELAY);
+    printf("received state %d times\n", state);
   }
-  
 }
 
 void app_main(void)
 {
   xTaskCreate(
-    &receiver,
-    "sender",
-    2048,
-    NULL,
-    2,
-    &receiverHandler);
+      &receiver,
+      "sender",
+      2048,
+      NULL,
+      2,
+      &receiverHandler);
 
   xTaskCreate(
-    &sender,
-    "receiver",
-    2048,
-    NULL,
-    2,
-    NULL);
-
+      &sender,
+      "receiver",
+      2048,
+      NULL,
+      2,
+      NULL);
 }
