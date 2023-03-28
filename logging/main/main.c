78bc0f3969f17341d703aea3c54fdc63d984889a
segin-GH <segin.bytes@gmail.com>
2023-03-28 21:07:29 +0530

    .

--- logging/main/main.c
+++ logging/main/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-# include <esp_log.h>
+#include <esp_log.h>
+
 
 void app_main(void)
 {
-    ESP_LOGE("LOG","THIS IS A ERROR");
-    ESP_LOGW("LOG","THIS IS A WARNING");
-    ESP_LOGI("LOG","THIS IS A INFO");
-    ESP_LOGD("LOG","THIS IS A DEBUG");
-    ESP_LOGV("LOG","THIS IS A VERBOSE")
+    esp_log_level_set("LOG", ESP_LOG_INFO);
+    ESP_LOGE("LOG", "THIS IS A ERROR MSG");
+    ESP_LOGW("LOG", "THIS IS A WARNING MSG");
+    ESP_LOGI("LOG", "THIS IS A INFO MSG");
+    ESP_LOGD("LOG", "THIS IS A DEBUG MSG");
+    ESP_LOGV("LOG", "THIS IS A VERBOSE MSG");
 }
