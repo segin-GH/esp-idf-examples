50d1860b201e8256ed6fbb0c90712252a4898a91
segin-GH <segin.bytes@gmail.com>
2023-04-07 15:45:17 +0530

    .

--- suspendAndResumeTask/main/main.c
+++ suspendAndResumeTask/main/main.c
@@ -1,6 +1,48 @@
-#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-void app_main(void)
+static TaskHandle_t receiverHandler = NULL;
+
+// Function that represents a sender task
+void senderTask(void *pvParameter)
 {
+    while (1)
+    {
+        printf("snd task\n");
+        vTaskDelay(pdMS_TO_TICKS(5000));
+        
+    }
+}
 
+// Function that represents a receiver task
+void receiverTask(void *pvParameter)
+{
+    while (1)
+    {
+        printf("rcv task\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+void app_main()
+{
+    // Create the sender task
+    xTaskCreate(senderTask, "senderTask", 4096, NULL, 1, NULL);
+
+    // Create the receiver task and get a handle to it
+    xTaskCreate(receiverTask, "receiverTask", 4096, NULL, 1, &receiverHandler);
+
+    // Suspend the receiver task
+    vTaskSuspend(receiverHandler);
+
+    // Resume the receiver task after 5 seconds
+    vTaskDelay(pdMS_TO_TICKS(5000));
+    vTaskResume(receiverHandler);
+
+    // Continue executing other code or tasks
+    while (1)
+    {
+        printf("main task\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
