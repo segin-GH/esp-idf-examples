5edd33641b827170d0c34f347ea92b9409bdd293
segin-GH <segin.bytes@gmail.com>
2023-04-18 17:57:23 +0530

    .

---
+++ i2cScanner/main/main.c
@@ -0,0 +1,68 @@
+#include <driver/i2c.h>
+#include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <stdio.h>
+#include "sdkconfig.h"
+
+#define SDA_PIN 27
+#define SCL_PIN 26
+
+static char tag[] = "i2cscanner";
+
+void task_i2cscanner(void *parms)
+{
+    ESP_LOGD(tag, ">> i2cScanner");
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = SDA_PIN;
+    conf.scl_io_num = SCL_PIN;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = 100000;
+    i2c_param_config(I2C_NUM_0, &conf);
+
+    i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
+
+    esp_err_t espRc;
+    printf("     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\n");
+    printf("00:         ");
+    for (int i = 0; i < 0x78; i++)
+    {
+        i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+        i2c_master_start(cmd);
+        i2c_master_write_byte(cmd, (i << 1) | I2C_MASTER_WRITE, 1 /* expect ack */);
+        i2c_master_stop(cmd);
+
+        espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10 / portTICK_PERIOD_MS);
+        if (i % 16 == 0)
+        {
+            printf("\n%.2x:", i);
+        }
+        if (espRc == 0)
+        {
+            printf(" %.2x", i);
+        }
+        else
+        {
+            printf(" --");
+        }
+        // ESP_LOGD(tag, "i=%d, rc=%d (0x%x)", i, espRc, espRc);
+        i2c_cmd_link_delete(cmd);
+    }
+    printf("\n");
+    printf("Scan Done\n");
+    vTaskDelete(NULL);
+}
+
+void app_main()
+{
+    xTaskCreatePinnedToCore(
+        task_i2cscanner,
+        "i2c scanner",
+        2048,
+        NULL,
+        10,
+        NULL,
+        APP_CPU_NUM);
+}
