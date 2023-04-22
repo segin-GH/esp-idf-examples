3d8da031c35da2ad337d57723bd72e6e145266c9
segin-GH <segin.bytes@gmail.com>
2023-04-22 21:59:41 +0530

    .

---
+++ twai/twai-recv/main/main.c
@@ -0,0 +1,41 @@
+#include "esp_log.h"
+#include "esp_system.h"
+#include "driver/twai.h"
+
+#define TAG "twai"
+
+#define TX_PIN GPIO_NUM_14
+#define RX_PIN GPIO_NUM_27
+
+void twai_receive_task(void *pvParameters)
+{
+    while (1) {
+        // Wait for a message to be received
+        twai_message_t message;
+        if (twai_receive(&message, pdMS_TO_TICKS(1000)) == ESP_OK) {
+            ESP_LOGI(TAG, "Received message: ID=0x%08x, DLC=%d, data=0x%02x%02x%02x%02x%02x%02x%02x%02x",
+                     message.identifier, message.data_length_code,
+                     message.data[0], message.data[1], message.data[2], message.data[3],
+                     message.data[4], message.data[5], message.data[6], message.data[7]);
+        }
+    }
+}
+
+void app_main()
+{
+    // Configure TWAI module
+    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(TX_PIN, RX_PIN, TWAI_MODE_NORMAL);
+    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
+    twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+
+    ESP_ERROR_CHECK(twai_driver_install(&g_config, &t_config, &f_config));
+    ESP_ERROR_CHECK(twai_start());
+
+    // Create task for receiving messages
+    xTaskCreate(twai_receive_task, "twai_receive_task", 4096, NULL, 10, NULL);
+
+    // Wait indefinitely
+    while (1) {
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
--- twai/twai-snd/main/main.c
+++ twai/twai-snd/main/main.c
@@ -4,42 +4,44 @@
 
 #define TAG "twai"
 
-#define TX_PIN GPIO_NUM_23
-#define RX_PIN GPIO_NUM_22
+#define TX_PIN GPIO_NUM_4
+#define RX_PIN GPIO_NUM_5
 
 void app_main()
 {
     // Configure TWAI module
     twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(TX_PIN, RX_PIN, TWAI_MODE_NORMAL);
-    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_DEFAULT(1000, 125, 125, 50, 5, 2000);
+    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
     twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
 
     ESP_ERROR_CHECK(twai_driver_install(&g_config, &t_config, &f_config));
     ESP_ERROR_CHECK(twai_start());
 
-    // Prepare and send message
-    twai_message_t message = TWAI_MESSAGE_DEFAULT(TWAI_MSG_FLAG_EXTD, 0x12345678, 8);
-    message.data[0] = 0x11;
-    message.data[1] = 0x22;
-    message.data[2] = 0x33;
-    message.data[3] = 0x44;
-    message.data[4] = 0x55;
-    message.data[5] = 0x66;
-    message.data[6] = 0x77;
-    message.data[7] = 0x88;
-
-    ESP_LOGI(TAG, "Sending message...");
-    ESP_ERROR_CHECK(twai_transmit(&message, pdMS_TO_TICKS(1000)));
-
-    // Wait for transmission to complete
-    ESP_LOGI(TAG, "Waiting for transmission to complete...");
-    twai_status_info_t status;
-    do
+    for (;;)
     {
-        ESP_ERROR_CHECK(twai_get_status_info(&status));
-    } while (status.state != TWAI_STATE_READY);
 
-    // Stop TWAI module and uninstall driver
+        // Prepare and send message
+        twai_message_t message;
+        message.identifier = 0xAAAA;
+        message.extd = 1;
+        message.data_length_code = 8;
+
+        message.data[0] = 0x11;
+        message.data[1] = 0x22;
+        message.data[2] = 0x33;
+        message.data[3] = 0x44;
+        message.data[4] = 0x55;
+        message.data[5] = 0x66;
+        message.data[6] = 0x77;
+        message.data[7] = 0x88;
+
+        ESP_LOGI(TAG, "Sending message...");
+        if (twai_transmit(&message, pdMS_TO_TICKS(1000)) == ESP_OK)
+            printf("Message queued for transmission\n");
+        else
+            printf("Failed to queue message for transmission\n");
+    }
+
     ESP_ERROR_CHECK(twai_stop());
     ESP_ERROR_CHECK(twai_driver_uninstall());
 
