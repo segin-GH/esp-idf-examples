998433d9447498f68600fd6fd048be93a3e97ba1
segin-GH <segin.bytes@gmail.com>
2023-06-20 11:24:48 +0530

    .

--- adc/main/main.c
+++ adc/main/main.c
@@ -6,12 +6,12 @@
 void app_main(void)
 {
     adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(ADC1_CHANNEL_0,ADC_ATTEN_DB_0);
-    
-    for(;;)
+    adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_0);
+
+    for (;;)
     {
         int val = adc1_get_raw(ADC1_CHANNEL_0);
-        printf("val is %d\n",val);
-        vTaskDelay(1000/portTICK_PERIOD_MS);
+        printf("val is %d\n", val);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
