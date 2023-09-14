037604a5173bf7764e0252f064b6dfd255844ea2
segin-GH <segin.bytes@gmail.com>
2023-09-14 13:06:33 +0530

    added a fucniton to gen a can msg

--- twaiComp/main/twaiComp.c
+++ twaiComp/main/twaiComp.c
@@ -5,6 +5,30 @@
 
 #define TAG "main"
 
+void generate_can_message(uint16_t id, uint8_t *data, uint8_t length)
+{
+    can_message_t can_tx_message;
+    can_tx_message.identifier = id;
+    can_tx_message.data_length_code = length;
+    can_tx_message.extd = 1;
+    can_tx_message.data[0] = data[0];
+    can_tx_message.data[1] = data[1];
+    can_tx_message.data[2] = data[2];
+    can_tx_message.data[3] = data[3];
+    can_tx_message.data[4] = data[4];
+    can_tx_message.data[5] = data[5];
+    can_tx_message.data[6] = data[6];
+    can_tx_message.data[7] = data[7];
+
+    if (can_transmit(&can_tx_message, portMAX_DELAY) == ESP_OK)
+    {
+        printf("[0x%02x]", can_tx_message.identifier);
+        print_can_msg_in_cool_8t(can_tx_message.data, 8);
+    }
+    else
+        ESP_LOGE(TAG, "Failed to queue message for transmission");
+}
+
 static void can_handle(void *args, esp_event_base_t base, int32_t event_id, void *event_data)
 {
     can_message_t *can_rx_message = (can_message_t *)event_data;
@@ -25,30 +49,25 @@ void send_can_message_in_main(void *pvParms)
 {
     for (;;)
     {
-        vTaskDelay(pdMS_TO_TICKS(2000));
-        can_message_t can_tx_message;
-        can_tx_message.identifier = 0x20;
-        can_tx_message.data_length_code = 8;
-        can_tx_message.extd = 1;
-        can_tx_message.data[0] = 0x11;
-        can_tx_message.data[1] = 0x22;
-        can_tx_message.data[2] = 0x33;
-        can_tx_message.data[3] = 0x44;
-        can_tx_message.data[4] = 0x55;
-        can_tx_message.data[5] = 0x66;
-        can_tx_message.data[6] = 0x77;
-        can_tx_message.data[7] = 0x88;
+        for (int i = 0; i < 1000; i++)
+        {
+            vTaskDelay(pdMS_TO_TICKS(30));
+            
+            uint8_t data[8] = {0x12, 0x00, 0xE0, 0x90, 0x43, 0x00, 0xBE, 0xA3};
+            generate_can_message(0x10, data, 8);
 
-        if (can_transmit(&can_tx_message, portMAX_DELAY) == ESP_OK)
-            ESP_LOGI(TAG, "Message queued for transmission");
-        else
-            ESP_LOGE(TAG, "Failed to queue message for transmission");
+            uint8_t data_2[8] = {0x12, 0x00, 0xF0, 0x23, 0x44, 0x00, 0x58, 0xA3};
+            generate_can_message(0x11, data_2, 8);
+
+            // // generate_can_message(0x41, data, 8);
+            // vTaskDelay(pdMS_TO_TICKS(30));
+        }
     }
 }
 
 void app_main(void)
 {
-    can_init(5, 4, pdMS_TO_TICKS(100));
+    can_init(4, 5, pdMS_TO_TICKS(100));
 
     can_register_can_handler(can_handle, NULL);
 
