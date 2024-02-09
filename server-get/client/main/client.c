6ebf13598e082a059ba51f4c325059d7deb00e1f
segin-GH <segin.bytes@gmail.com>
2024-02-09 13:46:17 +0530

    feat(server-get): updated get server now recives data

--- server-get/client/main/client.c
+++ server-get/client/main/client.c
@@ -7,8 +7,46 @@
 #include "wifi_connect.h"
 #include "nvs_flash.h"
 
-#define MAX_HTTP_RECV_BUFFER 512
+#define MAX_HTTP_OUTPUT_BUFFER 2048 // adjust as needed, but don't get too excited
+
 static const char *TAG = "HTTP_CLIENT";
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+    switch (evt->event_id)
+    {
+    // here's your event handler, pretending to be useful
+    case HTTP_EVENT_ERROR:
+        ESP_LOGE(TAG, "HTTP_EVENT_ERROR");
+        break;
+
+    case HTTP_EVENT_ON_CONNECTED:
+        ESP_LOGI(TAG, "HTTP_EVENT_ON_CONNECTED");
+        break;
+
+    case HTTP_EVENT_HEADER_SENT:
+        ESP_LOGI(TAG, "HTTP_EVENT_HEADER_SENT");
+        break;
+
+    case HTTP_EVENT_ON_HEADER:
+        ESP_LOGI(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+        break;
+
+    case HTTP_EVENT_ON_DATA:
+        if (esp_http_client_is_chunked_response(evt->client))
+            printf("%.*s\n", evt->data_len, (char *)evt->data);
+        break;
+
+    case HTTP_EVENT_ON_FINISH:
+        ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH");
+        break;
+
+    case HTTP_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+        break;
+    }
+    return ESP_OK;
+}
 
 static void print_data_in_hex(char *data, int length)
 {
@@ -22,42 +60,8 @@ static void print_data_in_hex(char *data, int length)
     }
     if (length % 16 != 0)
     {
-        printf("\n"); // Ensure we start a new line after the last line of data.
+        printf("\n");
     }
-}
-
-esp_err_t _http_event_handler(esp_http_client_event_t *evt)
-{
-    switch (evt->event_id)
-    {
-    // here's your event handler, pretending to be useful
-    case HTTP_EVENT_ERROR:
-        ESP_LOGI(TAG, "HTTP_EVENT_ERROR");
-        break;
-    case HTTP_EVENT_ON_CONNECTED:
-        ESP_LOGI(TAG, "HTTP_EVENT_ON_CONNECTED");
-        break;
-    case HTTP_EVENT_HEADER_SENT:
-        ESP_LOGI(TAG, "HTTP_EVENT_HEADER_SENT");
-        break;
-    case HTTP_EVENT_ON_HEADER:
-        ESP_LOGI(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
-        break;
-    case HTTP_EVENT_ON_DATA:
-        ESP_LOGI(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
-        if (!esp_http_client_is_chunked_response(evt->client))
-        {
-            print_data_in_hex(evt->data, evt->data_len);
-        }
-        break;
-    case HTTP_EVENT_ON_FINISH:
-        ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH");
-        break;
-    case HTTP_EVENT_DISCONNECTED:
-        ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
-        break;
-    }
-    return ESP_OK;
 }
 
 wifi_cred_t wifi_cred = {
@@ -67,17 +71,18 @@ wifi_cred_t wifi_cred = {
     .k_timeout = 10000,
 };
 
-void http_get_task(void *pvParameters)
+void http_client_get_task(void *pvParameters)
 {
-    char *url = "http://192.168.0.121/hello";
+    char output_buffer[MAX_HTTP_OUTPUT_BUFFER];
     esp_http_client_config_t config = {
-        .url = url,
+        .url = "http://192.168.0.103/ota/update",
         .event_handler = _http_event_handler,
-        .cert_pem = NULL,
+        .timeout_ms = 15000, // be patient, we're on the internet through sim7600 and it's slow as hell
     };
     esp_http_client_handle_t client = esp_http_client_init(&config);
 
     esp_err_t err = esp_http_client_perform(client);
+
     if (err == ESP_OK)
     {
         ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %d",
@@ -111,5 +116,5 @@ void app_main()
         return;
     }
 
-    xTaskCreate(&http_get_task, "http_get_task", 8192, NULL, 5, NULL);
+    xTaskCreate(&http_client_get_task, "http_client_get_task", 4096, NULL, 5, NULL);
 }
