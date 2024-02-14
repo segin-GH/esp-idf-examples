2aac474adc06277df38628cccae626b95a3d41dd
segin-GH <segin.bytes@gmail.com>
2024-02-14 15:51:56 +0530

    style: clean up

--- NVS-rw/main/main.c
+++ NVS-rw/main/main.c
@@ -132,8 +132,6 @@ void app_main()
     err = nvs_flash_init();
     if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND)
     {
-        // NVS partition was truncated and needs to be erased
-        // Retry nvs_flash_init
         ESP_ERROR_CHECK(nvs_flash_erase());
         err = nvs_flash_init();
     }
