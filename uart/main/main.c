1504068800dce7d7f8775eb52f1655e6d2dfc42b
segin-GH <segin.bytes@gmail.com>
2024-05-09 11:05:19 +0530

    style: formated the code

--- uart/main/main.c
+++ uart/main/main.c
@@ -1,48 +1,48 @@
-#include <stdio.h>
-#include "esp_log.h"
 #include "driver/uart.h"
+#include "esp_log.h"
 #include "string.h"
+#include <stdio.h>
 
-#define TXD_PIN 4
-#define RXD_PIN 5
+#define TXD_PIN 17
+#define RXD_PIN 16
 
-#define RX_BUFc_SIZE 1024
+#define RX_BUF_SIZE 1024
 
 void app_main(void)
 {
-    uart_config_t uart_config = {
-        .baud_rate = 9600,
-        .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE};
-    uart_param_config(UART_NUM_1, &uart_config);
-    uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
-    uart_driver_install(UART_NUM_1, RX_BUF_SIZE, 0, 0, NULL, 0);
+    uart_config_t uart_config = {.baud_rate = 115200,
+                                 .data_bits = UART_DATA_8_BITS,
+                                 .parity = UART_PARITY_DISABLE,
+                                 .stop_bits = UART_STOP_BITS_1,
+                                 .flow_ctrl = UART_HW_FLOWCTRL_DISABLE};
 
-    char message[] = "ping";
-    printf("sending: %s\n", message);
-    uart_write_bytes(UART_NUM_1, message, sizeof(message));
+    uart_param_config(UART_NUM_2, &uart_config);
+    uart_set_pin(UART_NUM_2, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    uart_driver_install(UART_NUM_2, RX_BUF_SIZE, 0, 0, NULL, 0);
 
-    char incoming_message[RX_BUF_SIZE];
-    memset(incoming_message, 0, sizeof(incoming_message));
-    uart_read_bytes(UART_NUM_1, (uint8_t *)incoming_message, RX_BUF_SIZE, pdMS_TO_TICKS(500));
-    printf("received: %s\n", incoming_message);
+    char message[] = "ping\n";
+
+    for (;;)
+    {
+        printf("sending: %s\n", message);
+        uart_write_bytes(UART_NUM_2, message, sizeof(message));
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
 
 /*
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_system.h"
-#include "nvs_flash.h"
 #include "driver/uart.h"
-#include "freertos/queue.h"
 #include "esp_log.h"
+#include "esp_system.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
+#include "nvs_flash.h"
 #include "soc/uart_struct.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define ECHO_TEST_TXD  (4)
 #define ECHO_TEST_RXD  (5)
@@ -91,17 +91,17 @@ void app_main()
 */
 
 /*
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_system.h"
-#include "nvs_flash.h"
 #include "driver/uart.h"
-#include "freertos/queue.h"
 #include "esp_log.h"
+#include "esp_system.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
+#include "nvs_flash.h"
 #include "soc/uart_struct.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define ECHO_TEST_TXD  (4)
 #define ECHO_TEST_RXD  (5)
