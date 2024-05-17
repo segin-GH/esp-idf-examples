f8eee7622673e4edbb7e02b097e920ebda5b914b
segin-GH <segin.bytes@gmail.com>
2024-05-17 12:20:46 +0530

    feat: added crc32 gen function

--- CRC/main/main.c
+++ CRC/main/main.c
@@ -1,6 +1,18 @@
+#include "esp_crc.h"
 #include <stdio.h>
+#include <string.h>
 
-void app_main(void)
+void calculate_crc32()
 {
+    const char *data = "change";
+    size_t length = strlen(data);
 
+    uint32_t crc = esp_crc32_le(0, (const uint8_t *)data, length);
+
+    printf("crc32 of '%s' is 0x%08x\n", data, crc);
+}
+
+void app_main()
+{
+    calculate_crc32();
 }
